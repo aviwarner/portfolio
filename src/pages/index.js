db92c566af2f36826309ae03d707022c6698ded7
Avi Warner <aviwarner@gmail.com>
2020-11-06 08:19:27 -0800

    convert to sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import ContactBar from "../components/contactBar"
 import SEO from "../components/seo"
 
-import './styles.css';
+import './styles.scss';
 
 const IndexPage = () => {
   console.log('%cHi friend, watcha looking for?', 'font-weight: bold;font-size: 50px;color: red;text-shadow:3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
