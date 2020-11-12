dce9f035f7f69ec749935edcb3e692e77a5124a2
Avi Warner <aviwarner@gmail.com>
2020-11-12 10:25:48 -0800

    css fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { GiCarrier } from 'react-icons/gi';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import ContactBar from "../components/contactBar"
 import SEO from "../components/seo"
 
